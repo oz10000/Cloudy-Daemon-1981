950237a79adcb7c7c1b208a4052da0839e191581
oz10000 <ozdacat@gmail.com>
2026-09-01 20:13:41 -0300

    Update __init__.py

--- src/certification/__init__.py
+++ src/certification/__init__.py
@@ -1,5 +1,1 @@
-# src/certification/__init__.py
-from .certifier import Certifier
-from .module_tests import ModuleTests
-from .exchange_tests import ExchangeTests
-from .certificate_generator import CertificateGenerator
+
